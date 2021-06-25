7ba103415df34f8db9d407392c4ad36714ce71b3
Melina-w <73808841+Melina-w@users.noreply.github.com>
2021-06-25 13:45:23 +1200

    small fixes

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
   return (
     <div className="p-4">
       <h1 className="text-pink-600 text-3xl">If this is pink, tailwind is working</h1>
-      <Route path='/list' component={List}/>
+      <Route exact path='/list' component={List}/>
     </div>
   )
 }
