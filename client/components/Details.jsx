d72963b292a6d4de94f63def6299cf406234141d
bosh-code <ryan.julian.bosher@icloud.com>
2021-06-25 15:29:15 +1200

    add some styles for details

--- client/components/Details.jsx
+++ client/components/Details.jsx
@@ -1,9 +1,7 @@
-import React, { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { Link, useParams } from 'react-router-dom'
 
 import getPokeDetailById from '../apiDetails'
-
-
 
 function Details () {
   // console.log(getPokeDetailById('charmander'))
@@ -15,41 +13,65 @@ function Details () {
 
   useEffect(() => {
     getPokeDetailById(name)
-    .then(pokemon => {
-      // console.log('pokemon', pokemon)
-      setPokemon(pokemon)
-      return null
-    })
-    .catch(err => {
-      console.error(err)
-    })
+      .then(pokemon => {
+        // console.log('pokemon', pokemon)
+        setPokemon(pokemon)
+        return null
+      })
+      .catch(err => {
+        console.error(err)
+      })
   }, [name])
 
-pokemon.id && console.log(pokemon)
+  pokemon.id && console.log(pokemon)
 
-   return (
-    //  pokemon.id && 
-      <div>
-      <h1>#{pokemon.id}</h1>
-      <h1>{pokemon.name}</h1>
-      <img src={`http://img.pokemondb.net/artwork/large/${pokemon.name}.jpg`} />
-      <h1>{pokemon.weight} pokegrams</h1>
-      <h1>{pokemon.height} pokemetres</h1>
-      <h1>Types:</h1>
-      {pokemon.id &&
-        <ul>
-      {pokemon.types.map(type => {
-        return (
-            <li key={type.type.name}>
-              {type.type.name}
-            </li>
-        )
-      })}
-      </ul>
-    }
+  return (
+    //  pokemon.id &&
+    <div>
+
+      <div className="p-4">
+        <div className="bg-white rounded-lg shadow-xl border-2 border-green-800 md:flex m-12">
+          <img src={`https://img.pokemondb.net/artwork/large/${pokemon.name}.jpg`} alt={pokemon.name} className="h-60 w-60 p-1 rounded-t-lg md:rounded-l-lg md:rounded-t-none" />
+          <div className="p-6">
+            <h1 className="font-bold text-lg md:text-3xl mb-2 text-orange-700">#{pokemon.id} - {String(pokemon.name).toUpperCase()}</h1>
+            <p>Pokegrams: {pokemon.weight}</p>
+            <p>Pokemeters: {pokemon.height}</p>
+            <p>Types:</p>
+            {pokemon.id &&
+            <ul>
+              {pokemon.types.map(type => {
+                return (
+                  <li key={type.type.name}>
+                    - {type.type.name}
+                  </li>
+                )
+              })}
+            </ul>
+            }
+          </div>
+        </div>
       </div>
-     
-   )
+
+      {/* <h1>#{pokemon.id}</h1> */}
+      {/* <h1>{pokemon.name}</h1> */}
+      {/* <img src={`http://img.pokemondb.net/artwork/large/${pokemon.name}.jpg`}/> */}
+      {/* <h1>{pokemon.weight} pokegrams</h1> */}
+      {/* <h1>{pokemon.height} pokemetres</h1> */}
+      {/* <h1>Types:</h1> */}
+      {/* {pokemon.id && */}
+      {/* <ul> */}
+      {/*  {pokemon.types.map(type => { */}
+      {/*    return ( */}
+      {/*      <li key={type.type.name}> */}
+      {/*        {type.type.name} */}
+      {/*      </li> */}
+      {/*    ) */}
+      {/*  })} */}
+      {/* </ul> */}
+      {/* } */}
+    </div>
+
+  )
 }
 
 export default Details
--- client/components/List.jsx
+++ client/components/List.jsx
@@ -23,31 +23,14 @@ const List = () => {
       <div className="text-center content-center justify-center items-center overflow-hidden p-4">
         <img src="pokedex.png" alt="pokedex" className="filter drop-shadow-md mx-auto flex" width="800px"/>
         <div className="p-4">
-          <div
-            className=" py-10 bg-gray-200 rounded-2xl">
-
+          <div className=" p-4 rounded-2xl">
             <div className="grid grid-cols-3 gap-4">
               {pokemonList.map(item => (
-
-                // <div className="w-full lg:w-1/2 p-3">
-                //   <div
-                //     className="flex flex-col lg:flex-row rounded overflow-hidden w-1/2 h-1/2 border shadow shadow-lg">
-                //     <img className="block h-auto w-full  flex-none bg-cover "
-                //          src={`https://www.smogon.com/dex/media/sprites/xy/${item.name}.gif`}/>
-                //     <div
-                //       className="bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
-                //       <div className="text-black font-bold text-xl mb-2 leading-tight">
-                //         Can life make you a bitter developer?
-                //       </div>
-                //     </div>
-                //   </div>
-                // </div>
-
                 <div key={item.name} className="p-4 bg-orange-200">
-                  <div className="bg-white rounded-lg shadow-xl md:flex">
+                  <div className="bg-white rounded-lg shadow-xl border-2 border-green-800 md:flex">
                     <img src={`https://www.smogon.com/dex/media/sprites/xy/${item.name}.gif`} alt={item.name} className="h-36 w-36 p-1 rounded-t-lg md:rounded-l-lg md:rounded-t-none" />
                     <div className="p-6">
-                      <h3 className="font-bold text-lg md:text-3xl mb-2 text-orange-700">{item.name.toUpperCase()}</h3>
+                      <Link to={`/details/${item.name}`} className="font-bold text-lg md:text-3xl mb-2 text-orange-700">{item.name.toUpperCase()}</Link>
                     </div>
                   </div>
                 </div>
@@ -64,9 +47,7 @@ const List = () => {
         </div>
       </div>
     </div>
-    </>
   )
 }
 
 export default List
-
