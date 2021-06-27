4e10b20e24b48ea92a3fb387abbdda2e686de182
bosh-code <ryan.julian.bosher@icloud.com>
2021-06-27 13:15:06 +1200

    added pixelated border to details page

--- client/components/Details.jsx
+++ client/components/Details.jsx
@@ -27,7 +27,7 @@ function Details () {
         <Link to="/list"><h2>👈 Pokedex</h2></Link>
       </div>
       <div className="py-20 px-60">
-        <div className="bg-white rounded-2xl shadow-2xl border-4 border-gold md:flex m-12 font-pokemon">
+        <div className="bg-white shadow-2xl pxl-border md:flex m-12 font-pokemon">
           <img src={`https://img.pokemondb.net/artwork/large/${pokemon.name}.jpg`} alt={pokemon.name} className="h-60 w-60 p-1 rounded-t-lg md:rounded-l-lg md:rounded-t-none" />
           <div className="p-6">
             <h1 className="font-bold text-lg md:text-3xl mb-2 text-orange-700">#{pokemon.id} - {String(pokemon.name).toUpperCase()}</h1>
