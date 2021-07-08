2d367512869cab42af9ad0e2ae4c471257787702
bosh-code <ryan.julian.bosher@icloud.com>
2021-07-08 15:43:23 +1200

    fix details lint issue in App.jsx

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,7 +1,6 @@
 import List from './List'
 import Home from './Home'
 import React from 'react'
-import Details from './Details'
 import Id from './Id'
 import { Route } from 'react-router-dom'
 
@@ -9,7 +8,6 @@ const App = () => {
   return (
     <div>
       <Route exact path="/" component={Home}/>
-      {/* <Route exact path="/details/:name" component={Details}/> */}
       <Route exact path="/id/:id" component={Id}/>
       <Route exact path="/pokedex" component={List}/>
     </div>
